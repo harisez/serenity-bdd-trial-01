8631456fc37750510e935997e49482c3b0c8ba11
Hariharasudhan R <harisez@gmail.com>
2017-09-03 23:38:17 -0700

    Initial Maven /Eclipse project with some dependencies filled in.

---
+++ ui/src/main/java/org/hari/serenity/trial/ui/App.java
@@ -0,0 +1,13 @@
+package org.hari.serenity.trial.ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
